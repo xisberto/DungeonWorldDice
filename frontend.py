04d18ea236ea1736833d6f7dc7f35487ad324a4c
Humberto Fraga <humbertofraga@gmail.com>
2022-10-07 18:34:41 -0300

    Loads the selected guild channel

--- frontend.py
+++ frontend.py
@@ -82,15 +82,16 @@ def callback():
     return redirect(url_for('.dashboard'))
 
 
-@app.route('/dashboard/', defaults={'id': None})
-@app.route('/dashboard/<id>')
-def dashboard(id=None):
+@app.route('/dashboard/', defaults={'guild_id': None, 'channel_id': None})
+@app.route('/dashboard/<guild_id>/<channel_id>')
+def dashboard(guild_id=None, channel_id=None):
     discord = make_session(token=session.get('oauth2_token'))
     g.user = discord.get(API_BASE_URL + '/users/@me').json()
     g.guilds = discord.get(API_BASE_URL + '/users/@me/guilds').json()
-    if id is not None:
+    if guild_id is not None:
         # Loads the specified guild
-        g.guild = id
+        g.guild = guild_id
+        g.channels = discord.get(API_BASE_URL + f'/users/@me/guilds/{guild_id}/channels')
     return render_template('dashboard.html')
 
 
