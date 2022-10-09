51b1331987d31d6f50aed1ef8a415d316ac68500
Humberto Fraga <humbertofraga@gmail.com>
2022-10-09 16:58:24 -0300

    Adjust Main class as extending Bot

--- main.py
+++ main.py
@@ -4,55 +4,42 @@ import os
 
 from discord import Intents, utils
 from discord.ext import commands
-from discord.ext.commands import ExtensionFailed
 from replit import db
 from scss import Compiler
 
 from frontend import frontend
 
 
-class Main:
-    def __init__(self):
+class Main(commands.Bot):
+    def __init__(self, command_prefix, intents: Intents):
+        super().__init__(command_prefix, intents=intents)
+
         log_handler = logging.StreamHandler()
         utils.setup_logging(handler=log_handler)
         self.logger = logging.getLogger("main")
         self.logger.info("Starting main class")
 
-        intents = Intents.default()
-        intents.message_content = True
-
-        self.bot = commands.Bot(
-            command_prefix=".",
-            intents=intents
-        )
-        self.bot.author_id = os.environ['AUTHOR_ID']
+        self.author_id = os.environ['AUTHOR_ID']
         extensions = [
             'cogs.cog_chars'
         ]
         for ext in extensions:
-            asyncio.run(self.load_extensions(ext))
+            asyncio.run(self.load_extension(ext))
 
-    def run(self):
+    def run_bot(self):
         token = os.environ.get("DISCORD_BOT_SECRET")
-        self.bot.run(token)
+        self.run(token)
 
-    @commands.Cog.listener()
     async def on_ready(self):
         self.logger.info("Bot Ready")
         if 'guilds' not in db.keys():
             db['guilds'] = {}
-        for guild in self.bot.guilds:
+        for guild in self.guilds:
             db['guilds'][guild.id] = {
                 'name': guild.name,
                 'channels': [ch.id for ch in guild.channels],
                 'members': [mem.id for mem in guild.members]
             }
-
-    async def load_extensions(self, extension):
-        try:
-            await self.bot.load_extension(extension)
-        except ExtensionFailed as ef:
-            print(ef)
 
 
 def compile_scss():
@@ -63,6 +50,8 @@ def compile_scss():
 
 if __name__ == '__main__':
     compile_scss()
-    main = Main()
+    ints = Intents.default()
+    ints.message_content = True
+    main = Main(command_prefix=".", intents=ints)
     frontend()
-    main.run()
+    main.run_bot()
